bbd7f8d4e5371d949d80376ecf2543b834cdb6e5
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-20 21:10:50 +0200

    basic structure

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // database
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
+//product router
+const productRouter = require("./routes/productRoutes");
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>File Upload Starter</h1>");
 });
+
+app.use("/api/v1/products", productRouter);
 
 // middleware
 app.use(notFoundMiddleware);
