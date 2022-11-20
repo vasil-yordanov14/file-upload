958e4d40957e0f8081d43354a5a49644e66d8695
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-20 23:14:46 +0200

    fileUpload package

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const fileUpload = require("express-fileupload");
+
 // database
 const connectDB = require("./db/connect");
 //product router
@@ -13,7 +15,9 @@ const productRouter = require("./routes/productRoutes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(express.static("./public"));
 app.use(express.json());
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("<h1>File Upload Starter</h1>");
