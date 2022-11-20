fb92c3168bec13769103344c1182d2760a03e4b8
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-20 21:11:00 +0200

    basic structure

---
+++ controllers/uploadsController.js
@@ -0,0 +1,7 @@
+const { StatusCodes } = require("http-status-codes");
+
+const uploadProductImage = async (req, res) => {
+  res.send("upload product image");
+};
+
+module.exports = { uploadProductImage };
