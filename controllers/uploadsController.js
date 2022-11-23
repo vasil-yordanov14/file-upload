f150986d0d67b1d270104bd598a5cda8525b08f9
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-23 19:02:22 +0200

    additional checks

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,23 @@
 const path = require("path");
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded");
+  }
   const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError("Please upload image smaller 1MB");
+  }
+
   const imagePath = path.join(
     __dirname,
     "../public/uploads/" + `${productImage.name}`
